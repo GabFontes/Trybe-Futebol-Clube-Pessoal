22e070ccfab91086369f5c8f768a559b256aa714
gab <gabrielfontes321@gmail.com>
2022-06-03 17:32:03 -0300

    feat: implementing create match tests

--- app/backend/src/tests/MatchesTests.test.ts
+++ app/backend/src/tests/MatchesTests.test.ts
@@ -2,12 +2,20 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import Matches from '../database/models/Matches';
+import Teams from '../database/models/Teams';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 
 import { Response } from 'superagent';
-import { matches, updateGoalsBodyPatch } from './Mocks/MatchesMocks/Matches';
+import {
+  matches,
+  updateGoalsBodyPatch,
+  searchedTeams,
+  matchBodyPost,
+  newMatchBody,
+  token
+} from './Mocks/MatchesMocks/Matches';
 
 chai.use(chaiHttp);
 
@@ -83,6 +91,41 @@ describe('Testando a rota Matches', () => {
       it('Verifica se a partida com o id 1 teve os gols alterados com o status 200, OK', () => {
         expect(chaiHttpResponse.body).to.be.deep.equal({ message: "The match 1 has been updated" });
         expect(chaiHttpResponse).to.have.status(200);
+      })
+    });
+  });
+
+  describe('/matches POST', () => {
+    describe('Rota matches cria as partidas corretamente', async () => {
+      before(async () => {
+        sinon
+          .stub(Teams, "findByPk")
+          .onCall(0)
+          .resolves(searchedTeams[0] as Teams)
+          .onCall(1)
+          .resolves(searchedTeams[1] as Teams)
+        sinon
+          .stub(Matches, 'create')
+          .resolves(newMatchBody as Matches)
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', token)
+          .send(matchBodyPost)
+      });
+
+      after(() => {
+        (Teams.findByPk as sinon.SinonStub).restore();
+        (Matches.create as sinon.SinonStub).restore();
+      });
+
+      it('Verifica se retorna uma nova partida com status 201, CREATED', () => {
+        const { token } = chaiHttpResponse.body;
+
+        expect(chaiHttpResponse).to.have.status(201);
+        expect(chaiHttpResponse.body).to.be.deep.equal(newMatchBody);
+        expect(token).to.be.equal(token)
       })
     });
   });
