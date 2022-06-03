68d1ad04e829a5e40c5604b1d2de3babf0220f10
gab <gabrielfontes321@gmail.com>
2022-06-02 23:46:04 -0300

    feat: implementing LoginTests and LoginMocks

---
+++ app/backend/src/tests/LoginTests.test.ts
@@ -0,0 +1,93 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import Users from '../database/models/Users';
+import chaiHttp = require('chai-http');
+import { users, loginBody, loginBodyWithoutEmail, loginBodyWithoutPassword } from './Mocks/LoginMocks/login';
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando a rota Login', () => {
+  let chaiHttpResponse: Response;
+
+  describe('/login POST', () => {
+    describe('Quando é feita a requisição corretamente', async () => {
+      before(async () => {
+        sinon
+          .stub(Users, "findOne")
+          .resolves(users[0] as Users);
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send(loginBody)
+      });
+
+      after(() => {
+        (Users.findOne as sinon.SinonStub).restore();
+      });
+
+      it('Retorna o corpo correto com o status 200, OK', () => {
+        const { user, token } = chaiHttpResponse.body;
+
+        expect(chaiHttpResponse).to.have.status(200)
+        expect(user.email).to.be.equal('admin@admin.com')
+        expect(user.role).to.be.equal('admin')
+        expect(user.username).to.be.equal('Admin')
+        expect(token).not.to.be.undefined;
+
+      });
+    });
+
+    describe('Quando a requisição é feita incorretamente', async () => {
+      before(async () => {
+        sinon
+          .stub(Users, "findOne")
+          .resolves(users[0] as Users);
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send(loginBodyWithoutEmail)
+      });
+
+      after(() => {
+        (Users.findOne as sinon.SinonStub).restore();
+      });
+
+      it('Quando a requisição é feita sem o email 400, BAD REQUEST', () => {
+        expect(chaiHttpResponse).to.have.status(400)
+        expect(chaiHttpResponse.body).to.be.deep.equal({
+          message: "All fields must be filled"
+        });
+      });
+
+      it('Quando a requisição é feita sem a senha 400, BAD REQUEST', () => {
+        before(async () => {
+          sinon
+            .stub(Users, "findOne")
+            .resolves(users[0] as Users);
+
+          chaiHttpResponse = await chai
+            .request(app)
+            .post('/login')
+            .send(loginBodyWithoutPassword)
+        });
+
+        expect(chaiHttpResponse).to.have.status(400)
+        expect(chaiHttpResponse.body).to.be.deep.equal({
+          message: "All fields must be filled"
+        });
+      });
+
+    });
+
+  });
+
+});
